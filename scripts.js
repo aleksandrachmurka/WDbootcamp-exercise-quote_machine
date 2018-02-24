e4821d91de7eacb1c84e206a2fcb140ce34d8d81
Aleksandra Krzywicka <aleksandrakrzywicka@gmail.com>
2018-02-24 13:56:00 +0100

    Fix bug

--- scripts.js
+++ scripts.js
@@ -7,18 +7,17 @@ $(document).ready(function() {
 
 	var tweetLink = "https://twitter.com/intent/tweet?text=";
 	var quoteUrl = "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1";
-	var prefix = "https://cors-anywhere.herokuapp.com/";
 
 	function getQuote() {
-	    $.getJSON(prefix + quoteUrl, createTweet);
+	    $.getJSON(quoteUrl, createTweet);
 	    $.ajaxSetup({ cache: false });
 	}
 
 	function createTweet(input) {
 	    var data = input[0];
-
 	    var quoteText = $(data.content).text().trim();
 	    var quoteAuthor = data.title;
+	    var tweetText = "Quote of the day - " + quoteText + " Author: " + quoteAuthor;
 
 	    if (tweetText.length > 140) {
 		    getQuote();
@@ -32,11 +31,12 @@ $(document).ready(function() {
 	    if (!quoteAuthor.length) {
 	        quoteAuthor = "Unknown author";
 	    }
+
+
 	}
 
 
 
-    var tweetText = "Quote of the day - " + quoteText + " Author: " + quoteAuthor;
 
 });
 
